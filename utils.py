bfd9128de3ff4775fb130142d8aa60eeeca95b6c
PrashantFiles1 <109992225+PrashantFiles1@users.noreply.github.com>
2023-01-31 13:47:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -385,7 +385,7 @@ async def get_shortlink(link):
 
     url = f'https://omegalinks.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
-              'url': url,
+              'url': alias,
               'format': 'json'
               }
 
@@ -397,8 +397,8 @@ async def get_shortlink(link):
                     return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://omegalinks.in/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://omegalinks.in/api?api={URL_SHORTNER_WEBSITE_API}&url={alias}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://omegalinks.in/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return f'https://omegalinks.in/api?api={URL_SHORTNER_WEBSITE_API}&url={alias}'
